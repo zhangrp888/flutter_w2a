840db78592615b1cb99b515382704239b6388ab7
zhangrp888 <153265953@qq.com>
2023-07-10 16:56:19 +0800

    更新

--- android/src/main/java/com/example/flutter_w2a/web2app/bean/advinfo.java
+++ android/src/main/java/com/example/flutter_w2a/web2app/bean/advinfo.java
@@ -1,9 +1,10 @@
 package com.huntmobi.web2app.bean;
+import com.alibaba.fastjson.annotation.JSONField;
 
 import java.util.List;
 
 public class advinfo {
-
+    @JSONField(name = "adv_data")
     private List<String> adv_data;
 
     public List<String> getAdv_data() {
--- android/src/main/java/com/example/flutter_w2a/web2app/bean/errorinfo.java
+++ android/src/main/java/com/example/flutter_w2a/web2app/bean/errorinfo.java
@@ -1,7 +1,8 @@
 package com.huntmobi.web2app.bean;
+import com.alibaba.fastjson.annotation.JSONField;
 
 public class errorinfo {
-
+    @JSONField(name = "error")
     private Error error;
 
     public Error getError() {
@@ -13,10 +14,15 @@ public class errorinfo {
     }
 
     public static class Error {
+        @JSONField(name = "message")
         private String message;
+        @JSONField(name = "code")
         private int code;
+        @JSONField(name = "type")
         private String type;
+        @JSONField(name = "error_user_title")
         private String error_user_title;
+        @JSONField(name = "error_user_msg")
         private String error_user_msg;
 
         public String getMessage() {
